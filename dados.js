abce9bd62c93334b07bdcadd0e7bc8b617794922
Gustavo Gregatti <150409479+gustavogregatti@users.noreply.github.com>
2024-09-07 19:43:12 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Marisa Monte",
+        descricao: "Marisa Monte é uma das maiores cantoras e compositoras brasileiras, reconhecida por sua voz marcante e por suas canções que transitam entre a MPB e o pop. Com uma carreira consolidada, Marisa é admirada por sua versatilidade musical e por sua capacidade de renovar a música brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Marisa_Monte",
+        tags: "mpb nacional cantora brasileira",
+    },
+    {
+        titulo: "Ivete Sangalo",
+        descricao: "Ivete Sangalo é uma das maiores cantoras brasileiras, conhecida por sua energia contagiante e por seus shows grandiosos. Ícone do axé music, Ivete possui uma legião de fãs e é uma das artistas brasileiras mais bem pagas.",
+        link: "https://pt.wikipedia.org/wiki/Ivete_Sangalo",
+        tags: "salvador carnaval pop nacional",
+    },
+    {
+        titulo: "Anitta",
+        descricao: "Anitta é uma cantora, compositora e atriz brasileira que alcançou fama internacional com seus hits que misturam ritmos brasileiros com elementos do pop internacional. Conhecida por sua versatilidade musical e por suas performances exuberantes, Anitta é uma das maiores representantes da música brasileira no cenário mundial.",
+        link: "https://pt.wikipedia.org/wiki/Anitta",
+        tags: "internacional carioca rio",
+    }
+]
+
+
+
